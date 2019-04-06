cb45a573d6a522659d33ff522b781adf87411251
bh4r4th <bh4r4th@gmail.com>
2019-04-06 16:42:07 -0700

    first commit

---
+++ src/main/java/com/candidate/chutesladders/ChutesLaddersApplication.java
@@ -0,0 +1,13 @@
+package com.candidate.chutesladders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChutesLaddersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChutesLaddersApplication.class, args);
+	}
+
+}
